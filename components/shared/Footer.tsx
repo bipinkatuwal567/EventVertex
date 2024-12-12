df0c14f719ae2078747bd8186dee8d23d3d248e3
bipinkatuwal567 <bipinkatuwal098@gmail.com>
2024-12-12 19:43:48 +0545

    add footer section

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer>
+      <div className="border-t flex-center wrapper flex flex-between flex-col sm:flex-row gap-4 p-5 text-center">
+        <Link href={"/"}>
+          <Image
+            src="/assets/images/logo.svg"
+            alt="logo"
+            width={128}
+            height={38}
+          />
+        </Link>
 
-export default Footer
+        <p>{new Date().getFullYear()} EventVertex. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
